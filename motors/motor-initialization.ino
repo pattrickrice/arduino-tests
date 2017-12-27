9e89d77ad63089588e041d5a69a578193cc514b3
pattrickrice <patrickrice20@gmail.com>
2017-12-26 23:29:17 -0800

    style adjustments

--- motors/motor-initialization.ino
+++ motors/motor-initialization.ino
@@ -1,4 +1,3 @@
-
 /*********************************************************************
  * Author: Patrick Rice
  * Date: 12/26/2017
@@ -12,12 +11,16 @@ Servo motors[] = {m1, m2, m3, m4};
 bool tested = false;
 
 
+
+/*********************************************************************
+ * Initialize the motors 
+*********************************************************************/
 void setup() {
   Serial.begin(9600);
 
   Serial.println("Test initializing...");
-  m1.attach(8);  //Specify here the pin number on which the signal pin of ESC is connected.
-  m1.write(30);   //ESC arm command. ESCs won't start unless input speed is less during initialization.
+  m1.attach(8);   // Specifies the  pin number on which the signal pin of ESC is connected.
+  m1.write(30);   // ESC arm command. ESCs won't start unless input speed is less during initialization.
 
   m2.attach(9);  
   m2.write(30);  
@@ -28,10 +31,15 @@ void setup() {
   m4.attach(11);  
   m4.write(30);   
 
-  delay(3000);            //ESC initialization delay.
+  delay(3000);    // ESC initialization delay.
 
 }
 
+/*********************************************************************
+ * Loop through all of the motors, turn slowly increase their speed.
+ * The goal of the test here is to make sure the escs are receiving 
+ * the input and the motors are reacting properly.  
+*********************************************************************/
 void loop() {
   int speed;
 
@@ -42,9 +50,11 @@ void loop() {
       speed = 40;
 
       Serial.println("Motor " + String(i+1) + ": Started");
-
+      
+      // speed can vary between 40-130, the higher the value, the 
+      // higher the speed.
       while(speed < 100){
-        motors[i].write(speed);    //Vary this between 40-130 to change the speed of motor. Higher value, higher speed.
+        motors[i].write(speed);   
         delay(1500);
         speed += 10;
       }
